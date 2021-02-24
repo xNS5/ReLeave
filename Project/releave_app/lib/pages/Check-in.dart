9b1860883670f47e90b37d51dd3c1b1afc462ae9
pratzm <masonpratz@gmail.com>
2021-02-23 16:49:28 -0800

    added sobriety counter class, example of use in Check-in.dart

--- Project/releave_app/lib/pages/Check-in.dart
+++ Project/releave_app/lib/pages/Check-in.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
-import 'SobrietyCounter.dart';
+import '../SobrietyCounter.dart';
 
 // ignore: camel_case_types
 class CheckInMain extends StatelessWidget {
