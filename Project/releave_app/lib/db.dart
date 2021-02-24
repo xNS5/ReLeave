188cb3d5fe489e81501904ea1c8713f4ac7be8c6
Michael Kennedy <michaelkennedyns5@gmail.com>
2021-02-23 20:51:26 -0800

    Creating database and objects

--- Project/releave_app/lib/db.dart
+++ Project/releave_app/lib/db.dart
@@ -1,35 +1,89 @@
-import 'dart:io';
-import 'package:path_provider/path_provider.dart';
+import "dart:io" as io;
+import "package:path/path.dart";
 import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
+
+
+class User {
+  final int id;
+  final String firstName;
+  final String lastName;
+  final String birthDate;
+  String startDate;
+  final String userName;
+  final Consumption c;
+
+  User(this.id, this.firstName, this.lastName, this.birthDate, this.userName, this.c);
+}
+
+class Journal{
+  final int id;
+  final String date;
+  final JournalContent content;
+  final Feelings feelings;
+  final CheckIn status;
+  final bool toReddit;
+  final String redditURL;
+
+  Journal(this.id, this.date, this.content, this.feelings, this.status, this.toReddit, this.redditURL);
+}
+
+class Feelings{
+  final int id;
+  final int happy;
+  final int sad;
+  final int anxious;
+  final int craving;
+  final int frustrated;
+  final int angry;
+
+  Feelings(this.id, this.happy, this.sad, this.anxious, this.craving, this.frustrated, this.angry);
+}
+
+class CheckIn{
+  final int id;
+  final String date;
+  final bool checkin;
+
+  CheckIn(this.id, this.date, this.checkin);
+}
+
+class JournalContent{
+  final int id;
+  final String currentDate;
+  final String textContent;
+
+  JournalContent(this.id, this.currentDate, this.textContent);
+}
+
+class Consumption{
+  final String consumptionMethod;
+  final int amount;
+  final double money;
+
+  Consumption(this.consumptionMethod, this.amount, this.money);
+}
 
 
 
-class DBProvider{
-  static Database _database;
+class Sqlitedb {
+  static final Sqlitedb _instance = new Sqlitedb();
 
-  DBProvider._();
-  static final DBProvider db = DBProvider._();
+  factory Sqlitedb() => _instance;
+  static Database _db;
 
-  Future<Database> get database async{
-    if(_database != null){
-      return database;
+  Future<Database> get db async{
+    if(_db != null){
+      return _db;
     }
-    _database = await initDB();
-    return _database;
+    _db = await initdb();
+    return _db;
   }
 
-  initDB() async {
-    Directory documentsDirectory = await getApplicationDocumentsDirectory();
-    String path = (documentsDirectory.path + "TestDB.db");
-    return await openDatabase(path, version: 1, onOpen: (db) {
-    }, onCreate: (Database db, int version) async {
-      await db.execute("CREATE TABLE Client ("
-          "id INTEGER PRIMARY KEY,"
-          "first_name TEXT,"
-          "last_name TEXT,"
-          "blocked BIT"
-          ")");
-    });
+  initdb() async {
+    io.Directory documentDirectory = await getApplicationDocumentsDirectory();
+    String path = join(documentDirectory.path, "releave.db");
+    return (await openDatabase(path, version: 1));
   }
 }
 
