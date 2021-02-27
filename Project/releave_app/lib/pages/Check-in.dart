d67422a68156dc61d148d823daa181431d8ef1f7
Michael Kennedy <michaelkennedyns5@gmail.com>
2021-02-26 21:37:54 -0800

    Added lib to import

--- Project/releave_app/lib/pages/Check-in.dart
+++ Project/releave_app/lib/pages/Check-in.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:releave_app/lib.dart';
 
 // ignore: camel_case_types
 class CheckInMain extends StatelessWidget {
