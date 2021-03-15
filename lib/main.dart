b9f24300da733c13fbe32f661f0d0c5165b829d6
Michael Kennedy <michaelkennedyns5@gmail.com>
2021-03-14 23:12:56 -0700

    Changed button to navigate to reddit page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:releave_app/lib.dart';
+import 'package:releave_app/pages/RedditSubmission.dart';
 import 'package:sqflite/sqflite.dart';
 
 void main() {
@@ -9,10 +10,10 @@ void main() {
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    User user = new User.data("Michael", "Kennedy", "11/06/1997", "2021-01-01", "ReLeave-Dev");
-    SqlitedbHelper.db.insertUser(user).then((status){
-      if(status){
-        print("User inserted into database");
+    User u = new User.data("Michael", "Kennedy", "1997-06-11", "2021-01-01");
+    SqlitedbHelper.db.insertUser(u).then((state) => {
+      if(state){
+        print("User added")
       }
     });
     return MaterialApp(
@@ -34,42 +35,42 @@ class HomePage extends StatefulWidget {
   _HomeState createState() => _HomeState();
 }
 
-class _HomeState extends State<HomePage>{
+class _HomeState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: Sidebar(),
-      appBar: AppBar(
-        title: Text(widget.title),
-        actions: [
-          SobrietyCounter(),
-        ],
-      ),
-      body: Center(
-        child: Row(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget> [
+        drawer: Sidebar(),
+        appBar: AppBar(
+          title: Text(widget.title),
+          actions: [
+            SobrietyCounter(),
+          ],
+        ),
+        body: Center(
+              child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget> [
               MaterialButton(
-                onPressed: () {Navigator.push(context, MaterialPageRoute(builder: (context) => CheckInLogMain()));},
-                child: Text("Community\nCheck-In"),
-                color: Colors.blue,
-                splashColor: Colors.lightBlueAccent,
-                textColor: Colors.white,
-                padding: EdgeInsets.fromLTRB(50, 50, 50, 50),
-                shape: CircleBorder(),
+              onPressed: () {Navigator.push(context, MaterialPageRoute(builder: (context) => RedditSubmission()));},
+              child: Text("Community\nCheck-In"),
+              color: Colors.blue,
+              splashColor: Colors.lightBlueAccent,
+              textColor: Colors.white,
+              padding: EdgeInsets.fromLTRB(50, 50, 50, 50),
+              shape: CircleBorder(),
               ),
-             MaterialButton(
+              MaterialButton(
               onPressed: () {Navigator.push(context, MaterialPageRoute(builder: (context) => CheckInMain()));},
               child: Text("Personal\nCheck-In"),
-               color: Colors.green,
-               splashColor: Colors.lightGreenAccent,
-               textColor: Colors.white,
-               padding: EdgeInsets.fromLTRB(50, 50, 50, 50),
-               shape: CircleBorder(),
+              color: Colors.green,
+              splashColor: Colors.lightGreenAccent,
+              textColor: Colors.white,
+              padding: EdgeInsets.fromLTRB(50, 50, 50, 50),
+              shape: CircleBorder(),
               ),
-          ]
-            ),
-          )
-        );
+              ]
+              ),
+              )
+          );
+    }
   }
-}
