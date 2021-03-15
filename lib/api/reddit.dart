13a7db2af113e68665e0c891421b0afc7fcafca3
Michael Kennedy <michaelkennedyns5@gmail.com>
2021-03-14 23:56:55 -0700

    Figured out reddit api flow

--- lib/api/reddit.dart
+++ lib/api/reddit.dart
@@ -4,7 +4,19 @@ import 'package:dio/dio.dart';
 import 'package:draw/draw.dart';
 
 
-void main() async{
-  Reddit reddit = await Reddit.createScriptInstance(
-  );
+Future<void> main() async{
+  final userAgent = 'releave-test';
+  final configUri = Uri.parse('draw.ini');
+  final reddit = Reddit.createInstalledFlowInstance(userAgent: userAgent,
+      configUri: configUri);
+  final auth_url = reddit.auth.url(['*'], 'foobar');
+  print(auth_url);
+
+try{
+  await reddit.auth.authorize("Y7zUyKlP6u6_T1PZdEJ89d0rVffm3Q");
+}catch(e){
+  print(e.toString());
+}
+
+  print(await reddit.user.me());
 }
